f809781cef66cca12d7d66ffc4b0000c0f52fb09
borcescua <andraborcescu@gmail.com>
2021-05-14 10:28:22 +0300

    laborator 4

--- src/lab4/Problema3.java
+++ src/lab4/Problema3.java
@@ -6,8 +6,33 @@ import java.util.Scanner;
 public class Problema3 {
     // un sir de n elemente sortate crescator
     // se implementeaza o functie cu 2 param(un sir, un nr care sa fie cautat)
-    public static void main(String[] args) {
 
+    public static boolean binarySearch(int[] sir, int number) {
+        boolean a = false;
+        int position = -1;
+        int lowIndex = 0, highIndex = sir.length - 1;
+        while (lowIndex <= highIndex) {
+            int midIndex = (lowIndex + highIndex) / 2;
+            if (sir[midIndex] == number) {
+                a = true;
+                position = midIndex;
+                break;
+            }
+            if (sir[midIndex] > number)
+                highIndex = midIndex - 1;
+            if (sir[midIndex] < number)
+                lowIndex = midIndex + 1;
+        }
+        //System.out.println(a);
+        if (a == true) {
+            System.out.println("The index of number " + String.format("%s", number) + " is " + position);
+        } else {
+            System.out.println("The number " + String.format("%s", number) + " cannot be found, please enter another number!");
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
 
         int myArray[] = {0, 3, 1, 6, 10, 7, 20, 40, 15, 11};
         Arrays.sort(myArray);
@@ -16,31 +41,11 @@ public class Problema3 {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the number you would like to search for: ");
         int number = scan.nextInt();
-        CautareBinara(myArray, number);
+        binarySearch(myArray, number);
 
     }
-
-    static boolean CautareBinara(int[] sir, int number) {
-        boolean a = false;
-        int sol = -1;
-        int Left = 0, Right = sir.length - 1;
-        while (Left <= Right) {
-            int Mid = (Left + Right) / 2;
-            if (sir[Mid] == number) {
-                a = true;
-                sol = Mid;
-                break;
-            }
-            if (sir[Mid] > number)
-                Right = Mid - 1;
-            if (sir[Mid] < number)
-                Left = Mid + 1;
-        }
-        System.out.println(a);
-        if (a == true)
-            System.out.println("The number position is " + sol);
-        return a;
-
-    }
-
 }
+
+
+
+
