b7be44034dcffb5429732023f6393fde9a118b73
borcescua <andraborcescu@gmail.com>
2021-05-20 14:18:27 +0300

    laborator 5

---
+++ src/lab4/Demoo.java
@@ -0,0 +1,16 @@
+package lab4;
+
+public class Demoo {
+
+    public static void main(String[] args) {
+        Integer a1 = Integer.decode("10");
+        Integer a2 = Integer.valueOf("20");
+        Integer a3 = new Integer("5");
+        System.out.println(a1+a2+a3);
+
+        double b = 1_34.9;
+        System.out.println(b);
+
+
+    }
+}
