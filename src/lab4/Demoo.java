18ebc9e6a26e698944adf05e223457c3dbaf69a5
borcescua <andraborcescu@gmail.com>
2021-05-20 14:22:38 +0300

    integer

--- src/lab4/Demoo.java
+++ src/lab4/Demoo.java
@@ -4,12 +4,14 @@ public class Demoo {
 
     public static void main(String[] args) {
         Integer a1 = Integer.decode("10");
-        Integer a2 = Integer.valueOf("20");
+        Integer a2 = Integer.valueOf("200");
         Integer a3 = new Integer("5");
-        System.out.println(a1+a2+a3);
+        Integer a4 = new Integer("10");
+        System.out.println(a1+a2+a3+a4);
 
         double b = 1_34.9;
         System.out.println(b);
+
 
 
     }
